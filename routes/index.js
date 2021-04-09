2b52455aab9d826980b96ca03bae5f99f712dc7f
Tobias Holtdirk <tobias.holtdirk@mailbox.org>
2021-04-09 23:31:53 +0200

    Fix merge again

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,11 @@ const express = require('express')
 const Player = require('../public/javascripts/player')
 const router = express.Router()
 
-<<<<<<< HEAD
 const Torres = require('../public/javascripts/torres')
 const torres = new Torres()
 const player1 = new Player(torres, 0)
 const player2 = new Player(torres, 1)
 
-=======
->>>>>>> websocket
 router.get('/', function (req, res) {
   res.send(torres.html())
 })
@@ -20,7 +17,6 @@ router.get('/api', function (req, res) {
 })
 
 router.post('/api', function (req, res) {
-<<<<<<< HEAD
   let success = false
   console.log('Received action : ' + JSON.stringify(req.body))
   const player = req.body.player ? player2 : player1
@@ -35,11 +31,6 @@ router.post('/api', function (req, res) {
   } else { console.log('unknown action') }
   console.log(success ? 'action performed' : 'action could not be performed')
   res.send(torres.ascii())
-=======
-  console.log('Received move : ' + JSON.stringify(req.body))
-  torres.placeBlock(req.body.x, req.body.y)
-  res.json(req.body)
->>>>>>> websocket
 })
 
 module.exports = router
