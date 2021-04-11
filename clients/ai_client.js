dd9cb20716d46197b89eb2e7816cf5f298c28c53
hannahol <hanna.holtdirk@web.de>
2021-04-11 23:21:40 +0200

    feat(client): let ai only consider reasonable moves

--- clients/ai_client.js
+++ clients/ai_client.js
@@ -14,11 +14,10 @@ async function myMove () {
     messageParser.once('game_state_response', (data) => resolve(Torres.assignInstances(data)))
   })
   const t0 = performance.now()
-  // const bestMove = minimax(torres, torres.activePlayer, 5, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY)
   const bestMove = minimax(torres, torres.activePlayer, 5)
   const t1 = performance.now()
   console.log('time: ' + (t1 - t0) + 'ms')
-  // console.log(bestMove)
+
   if (bestMove.move) {
     send('move', bestMove.move)
   } else {
@@ -27,27 +26,19 @@ async function myMove () {
 }
 
 function minimax (torres, playerId, depth) {
-  // const t0 = performance.now()
-  const legalMoves = torres.getLegalMoves(torres.activePlayer)
-  // const t1 = performance.now()
-  // console.log('time getLegalMoves for depth ' + depth + ': ' + (t1 - t0) + 'ms')
-  // console.log(legalMoves)
-  // legalMoves = legalMoves.sort(function (a, b) { return 0.5 - Math.random() }) // sort randomly
-  if (depth === 0 || legalMoves.length === 0) {
+  const moves = torres.getReasonableMoves(torres.activePlayer)
+  if (depth === 0 || moves.length === 0) {
     return { move: null, value: torres.getPoints(playerId) }
   }
   let bestMove
   if (torres.activePlayer === playerId) {
     let maxValue = Number.NEGATIVE_INFINITY
-    for (const move of legalMoves) {
+    for (const move of moves) {
       let info = {}
       if (move.action === 'turn_end') info = torres.getInfo()
-      // console.log(torres._board)
       makeMove(torres, move, torres.activePlayer)
-      // console.log(move)
       const childMove = minimax(torres, playerId, depth - 1)
       undoMove(torres, move, torres.activePlayer, info)
-      // console.log(torres._board)
       if (childMove.value > maxValue) {
         maxValue = childMove.value
         bestMove = { move, value: childMove.value }
@@ -56,14 +47,12 @@ function minimax (torres, playerId, depth) {
     return bestMove
   } else { // minimizing player
     let minValue = Number.POSITIVE_INFINITY
-    for (const move of legalMoves) {
+    for (const move of moves) {
       let info = {}
       if (move.action === 'turn_end') info = torres.getInfo()
-      // console.log(torres.getInfo())
       makeMove(torres, move, torres.activePlayer)
       const childMove = minimax(torres, playerId, depth - 1)
       undoMove(torres, move, torres.activePlayer, info)
-      // console.log(torres.getInfo())
       if (childMove.value < minValue) {
         minValue = childMove.value
         bestMove = { move, value: childMove.value }
@@ -72,52 +61,6 @@ function minimax (torres, playerId, depth) {
   }
   return bestMove
 }
-
-/*
-function minimax (torres, playerId, depth, alpha, beta) {
-  const legalMoves = torres.getLegalMoves(torres.activePlayer)
-  legalMoves.sort(function (a, b) { return 0.5 - Math.random() }) // sort randomly
-
-  if (depth === 0 || legalMoves.length === 0) {
-    return { move: null, value: torres.getPoints(playerId) }
-  }
-
-  const tmpTorres = Torres.assignInstances(JSON.parse(JSON.stringify(torres)))
-  let bestMove
-  if (torres.activePlayer === playerId) {
-    let maxValue = Number.NEGATIVE_INFINITY
-    for (const move of legalMoves) {
-      makeMove(torres, move, torres.activePlayer)
-      const childMove = minimax(torres, playerId, depth - 1, alpha, beta)
-      torres = Torres.assignInstances(JSON.parse(JSON.stringify(tmpTorres)))
-      if (childMove.value > maxValue) {
-        maxValue = childMove.value
-        bestMove = { move, value: childMove.value }
-      }
-      alpha = Math.max(alpha, childMove.value)
-      if (alpha >= beta) {
-        break
-      }
-    }
-    return bestMove
-  } else { // minimizing player
-    let minValue = Number.POSITIVE_INFINITY
-    for (const move of legalMoves) {
-      makeMove(torres, move, torres.activePlayer)
-      const childMove = minimax(torres, playerId, depth - 1, alpha, beta)
-      torres = Torres.assignInstances(JSON.parse(JSON.stringify(tmpTorres)))
-      if (childMove.value < minValue) {
-        minValue = childMove.value
-        bestMove = { move, value: childMove.value }
-      }
-      beta = Math.min(beta, childMove.value)
-      if (beta <= alpha) {
-        break
-      }
-    }
-  }
-  return bestMove
-} */
 
 function makeMove (torres, move, playerId) {
   switch (move.action) {
