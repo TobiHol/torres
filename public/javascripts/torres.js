f8516d2e14ec9dd32f5bfe0e18211bdb8345e0a4
hannahol <hanna.holtdirk@web.de>
2021-04-11 12:23:55 +0200

    fix(game-logic): check with player for legal moves

--- public/javascripts/torres.js
+++ public/javascripts/torres.js
@@ -148,23 +148,24 @@ class Torres {
     console.log(scorePerPlayer)
   }
 
-  getLegalMoves (playerId) {
+  getLegalMoves (playerId) {´
     const legalMoves = []
     if (this._activePlayer !== -1 && this._Players[this._activePlayer].id === playerId) {
+      const player = this._Players[playerId]
       if (this._phase > 0) {
         legalMoves.push({ action: 'turn_end' })
       }
       for (let x = 0; x < this._board.width; x++) {
         for (let y = 0; y < this._board.height; y++) {
-          if (this._phase > 0 && this._board.canPlaceBlock(x, y)) {
+          if (this._phase > 0 && player.canPlaceBlock() && this._board.canPlaceBlock(x, y)) {
             legalMoves.push({ action: 'block_place', x, y })
           }
-          if (this._board.canPlaceKnight(x, y, playerId, this._phase === 0)) {
+          if (player.canPlaceKnight() && this._board.canPlaceKnight(x, y, playerId, this._phase === 0)) {
             legalMoves.push({ action: 'knight_place', x, y })
           }
           for (let destX = 0; destX < this._board.width; destX++) {
             for (let destY = 0; destY < this._board.height; destY++) {
-              if (this._phase > 0 && this._board.canMoveKnight(x, y, destX, destY, playerId)) {
+              if (this._phase > 0 && player.canMoveKnight() && this._board.canMoveKnight(x, y, destX, destY, playerId)) {
                 legalMoves.push({ action: 'knight_move', x, y, destX, destY })
               }
             }
